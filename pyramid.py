fe5c5206d1f6ef2c2c63e29122cf2834f6779f58
Hizbul Bahar <hizbul_ku09@yahoo.com>
2013-08-21 12:17:52 +0600

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -27,3 +27,16 @@
 				print("*" * (rows-i))
 
 		pyramid(6)
+		
+3. Write a program to generate a following structure:
+	*
+	**
+	***
+	****
+	*****
+ Solution:
+ 
+	def pyramid(rows):
+		for i in range(rows):
+			print ("*"*(i+1))
+	pyramid(6)
