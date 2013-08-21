c9a21d3e5d3c872b25de31a65b3b942bf61f66fe
Hizbul Bahar <hizbul_ku09@yahoo.com>
2013-08-21 11:05:41 +0600

    Update pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,14 @@
+1. Write a program to generate a following structure:
+	******
+	******
+	******
+	******
+	******
+	******
+
+  Solution:
+    def pyramid(rows):
+	    for i in range(rows):
+		    print("*" * 6)
+
+    pyramid(6)
